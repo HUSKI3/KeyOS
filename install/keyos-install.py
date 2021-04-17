01e86eb86e04ffd68938fe8e8ff4123f6afe1e74
Linkus <Check Discord>
2021-04-17 20:16:44 +0200

    Fixes

--- install/keyos-install.py
+++ install/keyos-install.py
@@ -45,21 +45,19 @@ print("Starting UI...")
 def setup_others():
 	print("You are about to install the themes additional packages, proceed?")
 	# Configs
-	cmd = "cp -r configs/* ~/.config/".split(" ")
-	cmd = subprocess.run(cmd, text=True)
-	if cmd.returncode != 0:
-		print(red+"[Error] An error occured while installing configuration files!"+reset)
+	os.system("cp -r configs/* ~/.config/")
 	# Touchegg
 	cmd = "sudo add-apt-repository ppa:touchegg/stable".split(" ")
 	cmd = subprocess.run(cmd, text=True)
 	if cmd.returncode != 0:
-		print(red+"[Error] An error occured while adding the touchegg repo!"+reset)
+		print(red+"[Warn] An error occured while adding the touchegg repo, but it's not important!"+reset)
 	# uhubctl
 	cmd = "git clone https://github.com/mvp/uhubctl".split(" ")
 	cmd = subprocess.run(cmd, text=True)
 	if cmd.returncode != 0:
 		print(red+"[Error] An error occured while cloning uhubctl!"+reset)
 	os.system("cd uhubctl && make")
+	print("Finished!")
 
 # Installation
 
